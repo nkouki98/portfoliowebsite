c41bb653785e44f0624cf725e0dea44d6d637a76
Syed Farhan Ahmed <45247245+nkouki98@users.noreply.github.com>
2023-11-02 20:01:43 -0600

    Update App.jsx
    test commit from macos

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,7 @@ import Layout from './components/Layout'
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [width, setWidth] = useState(window.innerWidth);
-  const breakpoint = 1024;
 
-
-
- useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
-     // subscribe to window resize event "onComponentDidMount"
-     window.addEventListener("resize", handleResizeWindow);
-     return () => {
-       // unsubscribe "onComponentDestroy"
-       window.removeEventListener("resize", handleResizeWindow);
-     };
-   }, []);
   return (
 
   
